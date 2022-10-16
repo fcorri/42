b994fabae96b90b551c582d7799fdc259c3af113
cphil98 <57729303+cphil-98@users.noreply.github.com>
2022-10-16 23:29:45 +0200

    ft_strlcat corretta

--- cursus/core/libft/ft_strlcat.c
+++ cursus/core/libft/ft_strlcat.c
@@ -3,12 +3,16 @@
 size_t	ft_strlcat(char *dst, const char *src, size_t size)
 {
 	size_t	d_len;
+	size_t	s_len;
+	int		cont;
 
 	d_len = ft_strlen(dst);
-	while (size - 1 > d_len && *src)
-		*(dst + d_len++) = *src++;
-	if (size - d_len - 1)
-		return (size);
+	s_len = ft_strlen(src);
+	cont = 0;
+	while (size > d_len + 1 + cont++ && *src)
+		*(dst++ + d_len) = *src++;
+	if (size < d_len + cont)
+		return (size + s_len);
 	*(dst + d_len) = '\0';
-	return (d_len + ft_strlen(src));
+	return (d_len + s_len);
 }
