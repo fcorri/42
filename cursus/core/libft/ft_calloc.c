2d621f2cc30b2b9f86c8e679a8c584c549eca2d1
corri-filippo <corrifilippo@me.com>
2022-11-26 10:52:49 +0100

    ft_calloc completata e testata
    La funzione passa tutti i test sul Mac, anche se non setta errno su ENOMEM. E' stata riscritta cosicché non venga chiamato malloc se c'è un overflow di size_t

--- cursus/core/libft/ft_calloc.c
+++ cursus/core/libft/ft_calloc.c
@@ -18,8 +18,10 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	char	*output;
 
 	buffer_len = nmemb * size;
-	output = malloc(buffer_len);
 	if (buffer_len < nmemb || buffer_len < size)
+		return (NULL);
+	output = malloc(buffer_len);
+	if (!output)
 		return (NULL);
 	ft_bzero(output, buffer_len);
 	return (output);
