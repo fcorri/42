4c8d3972fa67ac4a0619b4009464b0d6fd91594d
corri-filippo <corrifilippo@me.com>
2022-11-26 20:02:45 +0100

    ft_putnbr_fd completata e testata
    La funzione è stata implementata con una funzione ricorsiva accessoria, per non usare un array con lunghezza fissata a priori.

--- cursus/core/libft/ft_putnbr_fd.c
+++ cursus/core/libft/ft_putnbr_fd.c
@@ -12,11 +12,31 @@
 
 #include "libft.h"
 
+static void	ft_putnbr_fd_rec(long n, int fd)
+{
+	char	digit[1];
+
+	if (n == 0)
+		return ;
+	ft_putnbr_fd_rec(n / 10, fd);
+	digit[0] = '0' + (n % 10);
+	write(fd, digit, 1);
+}
+
 void	ft_putnbr_fd(int n, int fd)
 {
-	char	*input;
+	long	input;
 
-	input = ft_itoa(n);
-	write(fd, input, ft_strlen(input));
-	free(input);
+	if (n == 0)
+		write(fd, "0", 1);
+	else
+	{
+		input = n;
+		if (n < 0)
+		{
+			write(fd, "-", 1);
+			input *= -1;
+		}
+		ft_putnbr_fd_rec(input, fd);
+	}
 }
