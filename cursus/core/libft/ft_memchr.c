9ef57d6bd10f51b9cbc7f3b019d4d47df46a7e19
corri-filippo <corrifilippo@me.com>
2022-11-28 14:34:22 +0100

    ft_memchr migliorata e testata
    La funzione controlla sizeof(unsigned long) bytes alla volta invece di uno.

--- cursus/core/libft/ft_memchr.c
+++ cursus/core/libft/ft_memchr.c
@@ -12,25 +12,69 @@
 
 #include "libft.h"
 
+static unsigned long	ft_init_input(unsigned char c)
+{
+	unsigned long	input;
+
+	input = c | (c << 8);
+	input |= input << 16;
+	if (sizeof(unsigned long) > 4)
+		input |= ((input << 16) << 16);
+	return (input);
+}
+
+static unsigned long	ft_init_one(void)
+{
+	unsigned long	one;
+
+	one = 0x01 | (0x01 << 8);
+	one |= one << 16;
+	if (sizeof(unsigned long) > 4)
+		one |= ((one << 16) << 16);
+	return (one);
+}
+
+static void	ft_check_words(size_t *p_dest, size_t *p_n,
+	unsigned long input, unsigned long one)
+{
+	size_t			dest;
+	unsigned long	word;
+	size_t			quozient;
+	size_t			n;
+
+	dest = *p_dest;
+	n = *p_n;
+	quozient = n / sizeof(unsigned long);
+	while (quozient--)
+	{
+		word = *((unsigned long *) dest) ^ input;
+		word = ((word - one) & ~word) & (one << 7);
+		if (word != 0)
+			break ;
+		n -= sizeof(unsigned long);
+		dest += sizeof(unsigned long);
+	}
+	*p_dest = dest;
+	*p_n = n;
+}
+
 void	*ft_memchr(const void *s, int c, size_t n)
 {
-	const unsigned char	*p_s;
-	unsigned char		input;
-	size_t				index;
+	size_t			dest;
+	unsigned long	input;
+	unsigned long	one;
 
 	if (n != 0)
 	{
-		p_s = s;
-		input = c;
-		index = 0;
-		while (n && p_s[index] != input)
+		dest = (size_t) s;
+		input = ft_init_input(c);
+		one = ft_init_one();
+		ft_check_words(&dest, &n, input, one);
+		while (n--)
 		{
-			index++;
-			n--;
+			if (*((unsigned char *) dest++) == (unsigned char) c)
+				return ((void *)(dest - 1));
 		}
-		if (n == 0)
-			return (NULL);
-		return ((void *)(p_s + index));
 	}
 	return (NULL);
 }
