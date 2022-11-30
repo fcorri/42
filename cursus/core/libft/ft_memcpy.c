b96cca64d3c3e32889200e97e99bb6ec8c3eb94a
corri-filippo <corrifilippo@me.com>
2022-11-30 18:35:55 +0100

    ft_memcpy completata e testata
    La funzione gestisce il disallineamento leggendo words invece che bytes

--- cursus/core/libft/ft_memcpy.c
+++ cursus/core/libft/ft_memcpy.c
@@ -12,27 +12,57 @@
 
 #include "libft.h"
 
-void	*ft_memcpy(void *p_dest, const void *p_src, size_t n)
+static unsigned long	ft_read_word(size_t src)
+{
+	int	right;
+	int	left;
+
+	right = src % sizeof(unsigned long);
+	if (right == 0)
+		return (*(unsigned long *)src);
+	left = sizeof(unsigned long) - right;
+	return (
+		*(unsigned long *)(src - right) >> (right * 8)
+		| *(unsigned long *)(src + left) << (left * 8)
+	);
+}
+
+static size_t	ft_set_words(size_t *p_dest, size_t *p_src, size_t n)
 {
 	size_t	dest;
 	size_t	src;
 	size_t	quozient;
 
+	dest = *p_dest;
+	src = *p_src;
+	quozient = n / sizeof(unsigned long);
+	n -= quozient * sizeof(unsigned long);
+	while (quozient--)
+	{
+		*((unsigned long *) dest) = ft_read_word(src);
+		dest += sizeof(unsigned long);
+		src += sizeof(unsigned long);
+	}
+	*p_dest = dest;
+	*p_src = src;
+	return (n);
+}
+
+void	*ft_memcpy(void *p_dest, const void *p_src, size_t n)
+{
+	size_t	dest;
+	size_t	src;
+	size_t	remainder;
+
 	if (p_dest == p_src)
 		return (p_dest);
 	dest = (size_t) p_dest;
 	src = (size_t) p_src;
-	if (n >= sizeof(unsigned long))
-	{
-		quozient = n / sizeof(unsigned long);
-		n -= quozient * sizeof(unsigned long);
-		while (quozient--)
-		{
-			*((unsigned long *) dest) = *((unsigned long *) src);
-			dest += sizeof(unsigned long);
-			src += sizeof(unsigned long);
-		}
-	}
+	remainder = dest % sizeof(unsigned long);
+	n -= remainder;
+	while (remainder--)
+		*((unsigned char *) dest++) = *((unsigned char *) src++);
+	n = ft_set_words(&dest, &src, n);
 	while (n--)
 		*((unsigned char *) dest++) = *((unsigned char *) src++);
 	return (p_dest);
