80c4b69c649f5c70f3641b91657005a65d10aedc
corri-filippo <corrifilippo@me.com>
2022-12-27 12:22:52 +0100

    ft_calloc corrected

--- cursus/core/libft/ft_calloc.c
+++ cursus/core/libft/ft_calloc.c
@@ -19,7 +19,7 @@ void	*ft_calloc(size_t nmemb, size_t size)
 
 	buffer_len = nmemb * size;
 	if (buffer_len < nmemb || buffer_len < size)
-		buffer_len = -1;
+		buffer_len = 0;
 	output = malloc(buffer_len);
 	if (!output)
 		return (NULL);
