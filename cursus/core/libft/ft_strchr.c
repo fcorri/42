cc4dcbda1290445d22d76c453f544f28bbeb821a
cphil98 <57729303+cphil-98@users.noreply.github.com>
2022-12-31 20:26:54 +0100

    ft_strchr migliorata
    controlla words e non bytes

--- cursus/core/libft/ft_strchr.c
+++ cursus/core/libft/ft_strchr.c
@@ -12,16 +12,75 @@
 
 #include "libft.h"
 
-char	*ft_strchr(const char *s, int c)
+static void	ft_init(unsigned long *input, char c, unsigned long *one)
 {
-	char	input;
-	char	read;
+	unsigned long	word;
+	size_t			size;
 
-	input = c;
-	read = *s;
-	while (read && read != input)
-		read = *++s;
-	if (read == input)
-		return ((char *) s);
-	return (NULL);
+	size = sizeof(unsigned long);
+	word = c | (c << 8);
+	word |= (word << 16);
+	if (size > 4)
+		word |= ((word << 16) << 16);
+	*input = word;
+	word = 0x01010101L;
+	if (size > 4)
+		word |= ((word << 16) << 16);
+	*one = word;
+}
+
+static void	ft_checkword(char *src, unsigned long input, unsigned long one,
+	char **output)
+{
+	unsigned long	word;
+
+	word = (*(unsigned long *)src) ^ input;
+	word = ((word - one) & ~word) & (one << 7);
+	if (word != 0)
+	{
+		*output = src;
+		while (**output != (char) input)
+			(*output)++;
+	}
+}
+
+static char	*ft_check_words(char *src, char c)
+{
+	unsigned long	word;
+	unsigned long	input;
+	unsigned long	one;
+	char			*output;
+	char			*zero;
+
+	ft_init(&input, c, &one);
+	output = NULL;
+	zero = NULL;
+	while (1)
+	{
+		ft_checkword(src, input, one, &output);
+		if (c)
+			ft_checkword(src, '\0', one, &zero);
+		if (output && (output <= zero || !zero))
+			return (output);
+		else if (zero)
+			return (NULL);
+		src += sizeof(word);
+	}
+}
+
+char	*ft_strchr(const char *src, int c)
+{
+	char	word;
+	size_t	size;
+
+	size = sizeof(unsigned long);
+	while ((size_t) src % size)
+	{
+		word = *src++;
+		if (word == (char) c)
+			return ((char *)(src - 1));
+		if (word == '\0')
+			return (NULL);
+	}
+	return (ft_check_words((char *)src, c));
 }
