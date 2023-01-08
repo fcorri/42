5c68cf3445795bbc98f9feb0555255daeae8c54e
corri-filippo <corrifilippo@me.com>
2023-01-08 17:49:27 +0100

    ft_split: cambiata strategia
    Invece che usare liste, forse nemmeno pushata la versione, viene usata la tecnica del raddoppiamento di un array

--- cursus/core/libft/ft_split.c
+++ cursus/core/libft/ft_split.c
@@ -12,69 +12,105 @@
 
 #include "libft.h"
 
-static char	**ft_free_and_return(t_list *lst, int ok)
+static char	**ft_free_and_return_null(char **output)
 {
-	char	**output;
 	size_t	index;
-	t_list	*tmp;
+	char	*tmp;
 
-	if (ok)
-		output = malloc(sizeof(char *) * ((size_t)lst->content + 1));
-	index = 0;
-	tmp = lst->next;
-	free(lst);
-	lst = tmp;
-	while (lst)
+	index = 2;
+	tmp = output[index];
+	while (tmp)
 	{
-		if (ok)
-			output[index++] = lst->content;
-		tmp = lst->next;
-		free(lst);
-		lst = tmp;
+		free(tmp);
+		tmp = output[index];
 	}
-	if (!ok)
-		return (NULL);
-	output[index] = NULL;
+	free(output);
+	return (NULL);
+}
+
+static char	**ft_return(char **output)
+{
+	char	**tmp;
+	size_t	n;
+
+	n = (size_t) output[1] + 1;
+	tmp = output;
+	output = malloc(sizeof(char *) * n);
+	while (n--)
+		output[n] = tmp[2 + n];
+	free(tmp);
 	return (output);
 }
 
-static int	ft_lstadd_back_split(t_list **tail, t_list *node)
+static char	**ft_init_arr(void)
 {
-	if (!node)
-		return (0);
-	(**tail).next = node;
-	*tail = (**tail).next;
-	return (1);
+	char	**output;
+
+	output = malloc(sizeof(char *) * 3);
+	if (output)
+	{
+		output[0] = (char *) 1;
+		output[1] = 0;
+		output[2] = NULL;
+	}
+	return (output);
+}
+
+static void	ft_concat_arr(char ***output, char *next)
+{
+	char	**tmp;
+	size_t	len;
+	size_t	n;
+	size_t	index;
+
+	tmp = *output;
+	len = (size_t) tmp[0];
+	n = (size_t) tmp[1];
+	if (n + 1 > len - 1)
+	{
+		*output = malloc(sizeof(char *) * (len * 2 + 2));
+		if (!*output)
+			return ;
+		(*output)[0] = (char *)(len * 2);
+		(*output)[n + 2] = next;
+		index = 1;
+		while (++index < n + 2)
+			(*output)[index] = tmp[index];
+		(*output)[len + 2] = NULL;
+		free(tmp);
+	}
+	else
+		(*output)[n + 2] = next;
+	(*output)[1] = (char *) n + 1;
 }
 
 char	**ft_split(char const *s, char c)
 {
-	t_list	*head;
-	t_list	*tail;
-	char	*str;
+	char	**output;
 	size_t	index;
 	size_t	len;
+	char	*str;
 
 	if (!s)
 		return (NULL);
-	head = ft_lstnew(NULL);
-	tail = head;
+	output = ft_init_arr();
+	if (!output)
+		return (ft_free_and_return_null(output));
 	index = 0;
 	while (s[index])
 	{
-		str = ft_strchr(s + index, c);
-		if (str)
-			len = str - (s + index);
+		len = (size_t) ft_strchr(s + index, c);
+		if (len)
+			len -= ((size_t) s + index);
 		else
-			len = ft_strlen(s + index);
-		str = malloc(len + 1);
-		if(!str)
-			return (ft_free_and_return(head, 0));
-		ft_strlcpy(str, s + index, len + 1);
-		head->content = (int *)((size_t) head->content + 1);
-		if (!ft_lstadd_back_split(&tail, ft_lstnew(str)))
-			return (ft_free_and_return(head, 0));
+			len = (size_t) ft_strchr(s + index, '\0');
+		if (len)
+		{
+			str = malloc(sizeof(char) * (len + 1));
+			ft_strlcpy(str, s + index, len + 1);
+			ft_concat_arr(&output, str);
+		}
 		index += len + 1;
 	}
-	return (ft_free_and_return(head, 1));
+	return (ft_return(output));
 }
