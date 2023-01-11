016c1138fc14b63f8fc7b67744146e0109db57dc
Filippo Corri <fcorri@c2r3p5.42firenze.it>
2023-01-11 17:38:07 +0100

    Corretto errore in file .h
    Mancava #define BUFFERSIZE

--- cursus/core/get_next_line/get_next_line.h
+++ cursus/core/get_next_line/get_next_line.h
@@ -6,12 +6,15 @@
 /*   By: fcorri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/01/09 18:07:57 by fcorri            #+#    #+#             */
-/*   Updated: 2023/01/09 18:07:57 by fcorri           ###   ########.fr       */
+/*   Updated: 2023/01/11 17:37:42 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef GET_NEXT_LINE_H
 # define GET_NEXT_LINE_H
+# ifndef BUFFER_SIZE
+#  define BUFFER_SIZE 42
+# endif
 
 # include <unistd.h>
 # include <stdlib.h>
