8acebea463e0aa01319de1e3a5ab53b11cddf5a8
Filippo Corri <fcorri@c2r3p5.42firenze.it>
2023-02-01 14:45:01 +0100

    Ready to push get_next_line 2nd time

--- cursus/core/libft/ft_calloc.c
+++ cursus/core/libft/ft_calloc.c
@@ -5,24 +5,38 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcorri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2022/10/18 17:32:46 by fcorri            #+#    #+#             */
-/*   Updated: 2022/11/15 19:41:12 by fcorri           ###   ########.fr       */
+/*   Created: 2023/01/17 18:32:08 by fcorri            #+#    #+#             */
+/*   Updated: 2023/01/18 19:10:54 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft.h"
 
-void	*ft_calloc(size_t nmemb, size_t size)
+void	*ft_calloc(size_t nmemb, size_t len)
 {
+	size_t	output;
 	size_t	buffer_len;
-	char	*output;
 
-	buffer_len = nmemb * size;
-	if (buffer_len < nmemb || buffer_len < size)
-		buffer_len = 0;
-	output = malloc(buffer_len);
+	buffer_len = nmemb * len;
+	if (buffer_len && (buffer_len < nmemb || buffer_len < len))
+		buffer_len = -1;
+	output = (size_t) malloc(buffer_len);
 	if (!output)
 		return (NULL);
-	ft_memset(output, 0, buffer_len);
-	return (output);
+	if (!buffer_len)
+		return ((void *)output);
+	output += (buffer_len - 1);
+	buffer_len -= (output % sizeof(unsigned long) + 1);
+	while (output % sizeof(unsigned long))
+		*(char *)output-- = 0;
+	*(char *)output = 0;
+	while (buffer_len >= sizeof(unsigned long))
+	{
+		output -= sizeof(unsigned long);
+		*(unsigned long *)output = 0;
+		buffer_len -= sizeof(unsigned long);
+	}
+	while (buffer_len--)
+		*(char *)output-- = 0;
+	return ((void *)output);
 }
