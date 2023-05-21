9c742b3371f9be772c35be1d9e4bb376e9b5b19a
Wall-E <corrifilippo@gmail.com>
2023-05-21 22:14:44 +0200

    Project started

--- cursus/core/fdf/src/fdf.h
+++ cursus/core/fdf/src/fdf.h
@@ -6,31 +6,31 @@
 /*   By: fcorri <fcorri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/15 00:46:43 by fcorri            #+#    #+#             */
-/*   Updated: 2023/05/15 11:52:05 by fcorri           ###   ########.fr       */
+/*   Updated: 2023/05/21 22:11:55 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef FDF_H
 # define FDF_H
 
-# include <stdlib.h>
 # include <mlx.h>
 # include <X11/X.h>
 # include <X11/keysym.h>
 
+# include <errno.h>
+
 # include "libft/libft.h"
-# include "printf/ft_printf.h"
 
 # define MLX_ERROR 1
 # define WIDTH 1000
 # define HEIGHT 500
 
-
-typedef struct s_data
+typedef struct s_mlx
 {
-	void	*mlx_ptr;
-	void	*win_ptr;
-}	t_data;
+	void	*obj;
+	void	*win;
+}	t_mlx;
+
+int	ft_error(char *input);
 
 #endif
-
--- cursus/core/fdf/src/utils.c
+++ cursus/core/fdf/src/utils.c
@@ -1,32 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   main.c                                             :+:      :+:    :+:   */
+/*   utils.c                                            :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: fcorri <fcorri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/05/15 09:47:40 by fcorri            #+#    #+#             */
-/*   Updated: 2023/05/21 22:12:52 by fcorri           ###   ########.fr       */
+/*   Created: 2023/05/21 22:05:47 by fcorri            #+#    #+#             */
+/*   Updated: 2023/05/21 22:12:16 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf.h"
 
-int	ft_check_args(int argc, char **argv)
-{
-	return (0);
-}
-
-int main(int argc, char **argv)
-{
-	t_mlx	mlx;
-
-	// check args
-	if (ft_check_args(argc, argv))
-		exit(EXIT_FAILURE);
-	// check map
-	// init mlx
-	// set mlx hooks
-	// loop
-	return (0);
-}
+int	ft_error(char *input);
