ff725280047f883668de93aa7f8c5b0961eba1b6
filippo-MacBookAir-Ubuntu <corrifilippo@gmail.com>
2023-06-04 15:13:42 +0200

    ready to test bresenham's line algorithm

--- cursus/core/testing/src/main.c
+++ cursus/core/testing/src/main.c
@@ -6,17 +6,32 @@
 /*   By: fcorri <fcorri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/03 16:48:17 by fcorri            #+#    #+#             */
-/*   Updated: 2023/06/03 17:08:14 by fcorri           ###   ########.fr       */
+/*   Updated: 2023/06/03 17:31:33 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "libft/libft.h"
 #include <mlx.h>
 
+typedef struct	s_data
+{
+	void	*this;
+	char	*addr;
+	int		bpp;
+	int		ll;
+	int		endian;
+}	image;
+
+static void	ft_pixel_put(image *img, int x, int y, int color)
+{
+	*(unsigned int *)(img->addr + (y * img->ll + x * (img->bpp / 8))) = color;
+}
+
 int main(void)
 {
 	void	*mlx;
 	void	*win;
+	image	img;
 
 	mlx = mlx_init();
 	if (!mlx)
@@ -27,6 +42,16 @@ int main(void)
 		free(mlx);
 		return (1);
 	}
+	img.this = mlx_new_image(mlx, 1000, 500);
+	if (!img.this)
+	{
+		mlx_destroy_window(mlx, win);
+		free(mlx);
+		return (1);
+	}
+	img.addr = mlx_get_data_addr(img.this, &img.bpp, &img.ll, &img.endian);
+	ft_pixel_put(img, 50, 50, 0x00FF0000);
+	mlx_put_image_to_window(mlx, win, img.this, 0, 0);
 	mlx_loop(mlx);
 	return (0);
 }
