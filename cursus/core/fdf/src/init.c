7dfb0c9c4a1fdbfca0a8e46f2522e220255a15a1
filippo-MacBookAir-Ubuntu <corrifilippo@gmail.com>
2023-06-04 15:13:19 +0200

    Started reading the map

--- cursus/core/fdf/src/init.c
+++ cursus/core/fdf/src/init.c
@@ -6,18 +6,60 @@
 /*   By: fcorri <fcorri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/05/22 22:10:26 by fcorri            #+#    #+#             */
-/*   Updated: 2023/05/23 19:40:54 by fcorri           ###   ########.fr       */
+/*   Updated: 2023/06/01 20:49:45 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf_p.h"
 
+static void	ft_add_points(char **vector, t_map *map)
+{
+	char	*line;
+
+	line = *vector++;
+	while (line)
+	{
+		ft_lstadd_back(map->points, ft_lstnew(tmp));
+		line = *vector++;
+	}
+}
+
+static void	ft_init_points(int fd, t_map *map)
+{
+	int		x;
+	int		y;
+	char	*tmp;
+	char	**vector;
+	t_list	*last;
+
+	x = 0;
+	y = 0;
+	tmp = get_next_line(fd);
+	vector = ft_split(tmp, ' ');
+	tmp = *vector++;
+	last = map->points;
+	while (tmp)
+	{
+		last->next = ft_lstnew(tmp)
+		last = last->next;
+		x++;
+		tmp = *vector++;
+	}
+	map->x = x;
+	tmp = get_next_line(fd);
+	while (tmp)
+	{
+		ft_add_points(ft_split(tmp, ' '), map);
+		y++;
+		tmp = get_next_line(fd);
+	}
+	map->y = y;
+}
+
 t_map	*ft_init_map(char *filename)
 {
 	int		fd;
 	t_map	*map;
-	char	*line;
-	int		y;
 
 	fd = open(filename, O_RDONLY);
 	if (fd < 0)
@@ -25,16 +67,8 @@ t_map	*ft_init_map(char *filename)
 	map = malloc(sizeof(*map));
 	if (!map)
 		return(ft_null_error(strerror(errno)));
-	map->x = ft_init_map_x(get_next_line(fd));
-	y = 1;
-	line = get_next_line(fd);
-	while (line)
-	{
-		y++;
-		ft_init_points(map, ft_split(line, ' '));
-		line = get_next_line(fd);
-	}
-	map->y = y;
+	map->points = NULL;
+	ft_init_points(fd, map);
 	return (map);
 }
 
