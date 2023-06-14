6ddc7abb5b65634330be051401b28b26398a369c
filippo-MacBookAir-Ubuntu <corrifilippo@gmail.com>
2023-06-14 17:49:46 +0200

    Testing split

--- cursus/core/libft/testing/src/main.c
+++ cursus/core/libft/testing/src/main.c
@@ -6,12 +6,25 @@
 /*   By: fcorri <fcorri@student.42.fr>              +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/06/14 17:17:01 by fcorri            #+#    #+#             */
-/*   Updated: 2023/06/14 17:17:01 by fcorri           ###   ########.fr       */
+/*   Updated: 2023/06/14 17:49:15 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
+#include "libft/libft.h"
+
 int main(void)
 {
+	char	*tmp = "ciao  come  stai  ?";
+	char	**output = ft_split(tmp, ' ');
+	int		i = 0;
+	tmp = output[i++];
+	while (tmp)
+	{
+		ft_printf("%d:\t%s\n", i, tmp);
+		free(tmp);
+		tmp = output[i++];
+	}
+	free(output);
 	return (0);
 }
 
