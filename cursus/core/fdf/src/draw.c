aac1776e5ff66db582e82abff8b87cb6305ebb8e
Filippo Corri <fcorri@c2r3p5.42firenze.it>
2023-06-15 15:26:51 +0200

    Separating responsibilities in drawing functions

--- cursus/core/fdf/src/draw.c
+++ cursus/core/fdf/src/draw.c
@@ -1,27 +1,15 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   image.c                                            :+:      :+:    :+:   */
+/*   draw.c                                             :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
-/*   By: fcorri <fcorri@student.42.fr>              +#+  +:+       +#+        */
+/*   By: fcorri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2023/06/14 21:50:38 by fcorri            #+#    #+#             */
-/*   Updated: 2023/06/15 15:22:44 by fcorri           ###   ########.fr       */
+/*   Created: 2023/06/15 15:22:14 by fcorri            #+#    #+#             */
+/*   Updated: 2023/06/15 15:22:23 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "fdf_p.h"
 
-int	ft_init_image(t_mlx *mlx)
-{
-	t_image	*image;
 
-	image = mlx_new_image(mlx->this, WIDTH, HEIGHT);
-	if (!image)
-	{
-		mlx_destroy_window(mlx->this, mlx->win);
-		return (ft_error("MLX_NEW_IMAGE", strerror(errno)));
-	}
-	mlx->image = image;
-	return (1);
-}
