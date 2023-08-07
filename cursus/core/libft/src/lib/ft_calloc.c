2bdd2683d5222f3c55890f349d5cf5c55bfff1da
fcorri <corrifilippo@gmail.com>
2023-08-07 17:58:12 +0200

    Everything works the right way

--- cursus/core/libft/src/lib/ft_calloc.c
+++ cursus/core/libft/src/lib/ft_calloc.c
@@ -6,7 +6,7 @@
 /*   By: fcorri <marvin@42.fr>                      +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2022/10/18 17:32:46 by fcorri            #+#    #+#             */
-/*   Updated: 2023/03/12 19:50:49 by fcorri           ###   ########.fr       */
+/*   Updated: 2023/08/05 17:51:02 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,6 +15,7 @@
 void	*ft_calloc(size_t nmemb, size_t size)
 {
 	size_t	buffer_len;
+	size_t	word_size;
 	char	*output;
 
 	buffer_len = nmemb * size;
@@ -23,6 +24,9 @@ void	*ft_calloc(size_t nmemb, size_t size)
 	output = ft_malloc_soul(buffer_len);
 	if (!output)
 		return (NULL);
+	word_size = sizeof(unsigned long);
+	if (buffer_len % word_size != 0)
+		buffer_len = word_size * (buffer_len / word_size + 1);
 	ft_memset(output, 0, buffer_len);
 	return (output);
 }
