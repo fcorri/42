6965915e18cbb408920006588be4768053c06f56
Filippo Corri <fcorri@c2r3p5.42firenze.it>
2024-03-04 19:00:56 +0100

    ex00 modified

--- cursus/core/projects/cpp/cpp00/ex00/src/megaphone.cpp
+++ cursus/core/projects/cpp/cpp00/ex00/src/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: fcorri <fcorri@student.42firenze.it>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:01:36 by fcorri            #+#    #+#             */
-/*   Updated: 2024/03/04 16:55:11 by fcorri           ###   ########.fr       */
+/*   Updated: 2024/03/04 18:59:00 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -17,12 +17,14 @@ int	main(int argc, char **argv)
 	if (argc == 1)
 		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
 	else
+	{
 		for (int i = 1; i < argc; i++)
 		{
 			int j = 0;
 			for (char c=argv[i][j]; c; c=argv[i][++j])
 				std::cout << (char) std::toupper(c);
 		}
-	std::cout << std::endl;
+		std::cout << std::endl;
+	}
 	return 0;
 }
