576514a3933996f00f7141728a2d9a330fd260e5
Filippo Corri <fcorri@c2r3p5.42firenze.it>
2024-05-21 18:37:29 +0200

    small code cleanings

--- cursus/core/projects/cpp/cpp00/ex00/src/megaphone.cpp
+++ cursus/core/projects/cpp/cpp00/ex00/src/megaphone.cpp
@@ -6,7 +6,7 @@
 /*   By: fcorri <fcorri@student.42firenze.it>       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/03/04 13:01:36 by fcorri            #+#    #+#             */
-/*   Updated: 2024/03/04 18:59:00 by fcorri           ###   ########.fr       */
+/*   Updated: 2024/05/21 18:32:54 by fcorri           ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,16 +15,13 @@
 int	main(int argc, char **argv)
 {
 	if (argc == 1)
-		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
-	else
+		std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
+	else for (int i = 1; i < argc; i++)
 	{
-		for (int i = 1; i < argc; i++)
-		{
-			int j = 0;
-			for (char c=argv[i][j]; c; c=argv[i][++j])
-				std::cout << (char) std::toupper(c);
-		}
-		std::cout << std::endl;
+		int j = 0;
+		for (char c=argv[i][j]; c; c=argv[i][++j])
+			std::cout << (char) std::toupper(c);
 	}
+	std::cout << std::endl;
 	return 0;
 }
